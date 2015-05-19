b9d2803f945cc869986b298b4f0982d0bb8e1ad2
Stefano Mazzone <smazzone@osxltsmazzo.local>
2015-05-19 12:09:31 +0200

    Switch hello jenkins CI. how are you today?

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins CI');
+  res.send('hello jenkins CI. how are you today?');
 });
  
 app.listen(process.env.PORT || 5000);
